af2651272205b39b571412650240a2e81c6b89ea
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 17:35:34 +0000

    Added New and edit buttons for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,9 +6,9 @@ class PortfoliosController < ApplicationController
   def new
     @portfolio_item = Portfolio.new
   end
-  
-    def create
-    @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle, :body))
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -17,22 +17,22 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
-    end
-    
-  def edit
-    @portfolio_item = portfolio.find(params[:id])
   end
-  
-    def update
-      @portfolio_item = portfolio.find(params[:id])
-      
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
-      if @portfolio_item.update( params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
-    end
-  
+  end
+
 end
