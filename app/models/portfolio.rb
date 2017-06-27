4a375db66eaa7adf8993d790e4634c04884c424b
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:19:55 +0000

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, 
+                                reject_if: lamda { |attrs| attrs['name'].blank? }
   validates_presence_of :title, :body
   
   def self.angular
