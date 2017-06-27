c4aee74debded0e179732d63610704a6e3ea7287
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:43:18 +0000

    Implemented form configuration for nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies, 
-                                reject_if: lamda { |attrs| attrs['name'].blank? }
+
   validates_presence_of :title, :body
   
   def self.angular
