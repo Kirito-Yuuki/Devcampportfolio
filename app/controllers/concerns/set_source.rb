fc3e8e2348d608836cea8eb4473e96363814cf88
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 16:41:19 +0000

    Added nav bar and added default header content

--- app/controllers/concerns/set_source.rb
+++ app/controllers/concerns/set_source.rb
@@ -1,10 +1,11 @@
-Model SetSource
+module SetSource
   extend ActiveSupport::Concern
 
   included do 
     before_filter :set_source
   end
-    def set_source
-      session [:source] = params[:q] if params[:q]
-    end
+  
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,4 +19,5 @@ Bundler.require(*Rails.groups)
 module Devcampportfolio
   class Application < Rails::Application
     config.action_controller.permit_all_parameters = true
+  end
 end
