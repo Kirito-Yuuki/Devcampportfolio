2a254b57bbec260d8efca058ecb829e427541e51
Jordan Hudgens <jordan@devcamp.com>
2017-07-25 16:24:06 +0000

    Fixing issues with the code

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170718142100) do
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.string   "subtitle"
     t.text     "main_image"
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
@@ -73,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170718142100) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
